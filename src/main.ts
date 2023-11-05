805718b2c3f53d2c73e544fecf3b8d20f820ca8a
Kushal Hada <kus@tilde.team>
2023-11-04 21:19:30 -0400

    I don't understnd signals

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,27 @@ import "./style.css";
 import { signal, computed, effect } from "@preact/signals-core";
 const counter = signal(1);
 const result = computed(() => counter.value * 10);
+let randomSeed = signal(randomIntFromInterval(1, 9));
+let randomizedResult = computed(() => counter.value * randomSeed.value);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Signals in Vanilla JS</h1>
     <div class="card">
       <button type="button">count is <span class="counter">${counter.value}</span></button>
+      <button type="reset" id="reset">Reset</button>
     </div>
     <p>Result: <span class="counter">${counter.value}</span> * 10 = <span class="result">10</span></p>
+    <p>Randomized result: <span class="counter">${counter.value}</span> * ${randomSeed} = <span class="randomizedResult">10</span></p>
   </div>
 `;
 
 function setCounter() {
     const counterElements = document.querySelectorAll<HTMLSpanElement>(".counter");
     const resultElement = document.querySelector<HTMLSpanElement>(".result");
+    const randomizedResultElement = document.querySelector<HTMLSpanElement>(".randomizedResult");
 
-    if (!counterElements?.length || !resultElement) {
+    if (!counterElements?.length || !resultElement || !randomizedResultElement) {
         return;
     }
 
@@ -26,6 +31,11 @@ function setCounter() {
     });
 
     resultElement.innerText = result.value.toString();
+    randomizedResultElement.innerText = randomizedResult.value.toString();
+}
+
+function randomIntFromInterval(min: number, max: number) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 const button = document.querySelector<HTMLButtonElement>("button");
@@ -35,3 +45,12 @@ button?.addEventListener("click", () => {
 });
 
 effect(setCounter);
+
+const resetButton = document.getElementById("reset");
+
+resetButton?.addEventListener("click", () => {
+  counter.value = 0;
+  const newRandom = randomIntFromInterval(1, 9);
+  console.info({ newRandom });
+  randomSeed.value = newRandom;
+});
