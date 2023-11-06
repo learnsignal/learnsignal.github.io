f98bf01b3d008f5aeee8ed358a9be98cd188badb
Kushal Hada <kus@tilde.team>
2023-11-05 19:10:43 -0500

    build signals

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,13 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  setCounter(0);
+}
+
+export function randomIntFromInterval(min: number, max: number) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { randomIntFromInterval } from "./counter";
 import "./style.css";
 import { signal, computed, effect } from "@preact/signals-core";
 const counter = signal(1);
+const resetPressed = signal(false);
 const result = computed(() => counter.value * 10);
 let randomSeed = signal(randomIntFromInterval(1, 9));
 let randomizedResult = computed(() => counter.value * randomSeed.value);
@@ -9,48 +11,50 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Signals in Vanilla JS</h1>
     <div class="card">
-      <button type="button">count is <span class="counter">${counter.value}</span></button>
+      <button type="button" id="incrementCounter">count is <span class="counter">${counter.value}</span></button>
       <button type="reset" id="reset">Reset</button>
     </div>
     <p>Result: <span class="counter">${counter.value}</span> * 10 = <span class="result">10</span></p>
-    <p>Randomized result: <span class="counter">${counter.value}</span> * ${randomSeed} = <span class="randomizedResult">10</span></p>
+    <p>Randomized result: <span class="counter">${counter.value}</span> * ${randomSeed.value} = <span class="randomizedResult">10</span></p>
   </div>
 `;
 
 function setCounter() {
-    const counterElements = document.querySelectorAll<HTMLSpanElement>(".counter");
-    const resultElement = document.querySelector<HTMLSpanElement>(".result");
-    const randomizedResultElement = document.querySelector<HTMLSpanElement>(".randomizedResult");
+  const counterElements = document.querySelectorAll<HTMLSpanElement>(".counter");
+  const resultElement = document.querySelector<HTMLSpanElement>(".result");
+  const randomizedResultElement = document.querySelector<HTMLSpanElement>(".randomizedResult");
 
-    if (!counterElements?.length || !resultElement || !randomizedResultElement) {
-        return;
-    }
+  if (!counterElements?.length || !resultElement || !randomizedResultElement) {
+    return;
+  }
 
-    counterElements.forEach((element) => {
-        element.innerText = counter.value.toString();
-    });
+  counterElements.forEach((element) => {
+    element.innerText = counter.value.toString();
+  });
 
-    resultElement.innerText = result.value.toString();
-    randomizedResultElement.innerText = randomizedResult.value.toString();
+  resultElement.innerText = result.value.toString();
+  randomizedResultElement.innerText = randomizedResult.value.toString();
 }
 
-function randomIntFromInterval(min: number, max: number) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-const button = document.querySelector<HTMLButtonElement>("button");
+const button = document.getElementById("incrementCounter");
+const resetButton = document.getElementById("reset");
 
 button?.addEventListener("click", () => {
-    counter.value++;
+  counter.value++;
+});
+
+resetButton?.addEventListener("click", () => {
+  counter.value = 0;
+  randomSeed.value = randomIntFromInterval(1, 9);
+  console.info({ new: randomSeed.value });
+  resetPressed.value = true;
 });
 
 effect(setCounter);
 
-const resetButton = document.getElementById("reset");
-
-resetButton?.addEventListener("click", () => {
-  counter.value = 0;
-  const newRandom = randomIntFromInterval(1, 9);
-  console.info({ newRandom });
-  randomSeed.value = newRandom;
-});
+setInterval(() => {
+  if (resetPressed.value === false) {
+    counter.value = randomIntFromInterval(4, 9);
+    randomSeed.value = randomIntFromInterval(3, 6);
+  }
+}, 200)
